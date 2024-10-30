5c3643aab4d0153411aad869bc65d7a22a5e4a91
ciigann <maxvetrov2004@yandex.ru>
2024-10-30 12:14:39 +0500

    3 версия commit

--- app/src/main/java/com/example/hiking/MainActivity.java
+++ app/src/main/java/com/example/hiking/MainActivity.java
@@ -12,7 +12,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.hiking.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
-//3
+//4
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
